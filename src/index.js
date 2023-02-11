8d8e471cf53e19127d7a40768ad916888f760ccc
Jacob Maier <jacobamaier@gmail.com>
2023-02-10 16:42:14 -0800

    WIP: Display response for lack of input and initial styling.

--- src/index.js
+++ src/index.js
@@ -17,27 +17,30 @@ function calculateCurrency(value, exchangeRate) {
 }
 
 function printError(error) {
-  const errorMessage = `There was an error in this request ${error["error-type"]}`;
-  document.querySelector("#response-display").innerText = errorMessage;
+  document.querySelector("#response-display").innerText = error;
 }
 
 function printCurrency(response, value) {
-  /* eslint-disable no-console */
-  console.log(response);
-  console.log(value);
-  let result = response['conversion_rates'];
-  const responseDisplay = document.querySelector('#response-display');
-  let exchangeOutput = document.querySelector('#exchange-iso').value;
-  responseDisplay.value = calculateCurrency(value, result[exchangeOutput]);
-  console.log(responseDisplay.value);
-  /* eslint-enable no-console */
+  if (value !== null) {
+    let result = response["conversion_rates"];
+    const responseDisplay = document.querySelector("#response-display");
+    let exchangeInput = document.querySelector('#input-iso').value;
+    let exchangeOutput = document.querySelector("#exchange-iso").value;
+    responseDisplay.value = calculateCurrency(value, result[exchangeOutput]);
+    document.querySelector(
+      "#response-display"
+    ).innerText = `Your conversion from ${exchangeInput} to ${exchangeOutput} is ${responseDisplay.value}`;
+  } else {
+    document.querySelector(
+      "#response-display"
+    ).innerText = `Please enter a number to convert.`;
+  }
 }
 
 const handleForm = () => {
   const inputValue = document.querySelector("input").value;
   const inputIso = document.querySelector("#input-iso").value;
   const exchangeIso = document.querySelector("#exchange-iso").value;
-
   getCurrency(inputIso, inputValue, exchangeIso);
 };
 
